64031ca35a91c283f318a642eef2692a8e460657
Tilo Pfliegner <tilo.pfliegner@web.de>
2012-05-29 17:04:34 +0200

    Implemented an ExternalCommand Created HelloRevit.addin manifest file
    HelloRevit.addin must be copied to C:\ProgramData\Autodesk\Revit\Addins\2013
    HelloRevit.dll must be copied to C:\ProgramData\Autodesk\Revit\Addins\2013\HelloRevit

--- HelloRevit/Class1.cs
+++ HelloRevit/Class1.cs
@@ -12,7 +12,22 @@ using Autodesk.Revit.Attributes;
 
 namespace HelloRevit
 {
-    public class Class1
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+    public class Command : IExternalCommand
     {
+        #region IExternalCommand members
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+            ref string message, Autodesk.Revit.DB.ElementSet elements)
+        {
+            TaskDialog dlg = new TaskDialog(@"Hello Revit");
+            dlg.MainContent = @"Hello Revit";
+            dlg.MainInstruction = @"Say hello!";
+            dlg.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, @"Hello");
+            dlg.Show();
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+        #endregion
     }
 }

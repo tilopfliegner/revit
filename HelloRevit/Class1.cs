07000ae9ae6e218448e596188e861b55ddc05ecd
Tilo Pfliegner <tilo.pfliegner@web.de>
2012-05-29 16:17:01 +0200

    Added references to Revit API

--- HelloRevit/Class1.cs
+++ HelloRevit/Class1.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
 namespace HelloRevit
 {
     public class Class1
